0b71d55f82ff0caf546a14054ee49ccd76b8eb00
Andrey <adreygichan@gmail.com>
2025-10-02 19:31:06 +0300

    добавление бакета

--- backend/django_airbnb/settings.py
+++ backend/django_airbnb/settings.py
@@ -130,7 +130,7 @@ STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         "OPTIONS": {
-            "access_key": os.environ.get("SUPABASE_ACCESS_KEY"),
+            "access_key": os.environ.get("SUPABASE_SERVICE_ROLE_KEY"),
             "secret_key": os.environ.get("SUPABASE_SERVICE_ROLE_KEY"),
             "bucket_name": os.environ.get("SUPABASE_BUCKET_NAME"),
             "region_name": "us-east-1",  
